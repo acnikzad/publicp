171c5fde628f55d5a317fe1129ee03f34a3bdb44
Aria Nikzad <a.c.nikzad@gmail.com>
2022-05-06 17:51:12 -0700

    added random review generator

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,12 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :bathroom
+
+  def bathoom
+    Bathroom.find_by(id: bathroom_id)
+  end
+
+  def user
+    User.find_by(id: user_id)
+  end
 end
